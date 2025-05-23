b497c5dd2703dd44ffdf41d39c6e63b1b2057857
Ger <germancandradem@gmail.com>
2025-05-23 10:31:39 -0500

    Moved to LiveData, Improved UI

--- app/src/main/java/com/ger/loadproducts/ui/MainActivity.kt
+++ app/src/main/java/com/ger/loadproducts/ui/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
+import com.ger.loadproducts.domain.Product
 import com.ger.loadproducts.ui.theme.LoadProductsTheme
+import com.ger.loadproducts.viewmodel.ProductState
 import com.ger.loadproducts.viewmodel.ProductsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,10 +26,11 @@ class MainActivity : ComponentActivity() {
 
         enableEdgeToEdge()
         setContent {
-            val productState = viewModel.productState.collectAsState()
+            val productState = viewModel.productState.observeAsState()
+
             LoadProductsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ProductsScreen(productState.value, innerPadding)
+                    ProductsScreen(productState.value!!, innerPadding)
                 }
             }
         }
--- app/src/main/java/com/ger/loadproducts/viewmodel/ProductsViewModel.kt
+++ app/src/main/java/com/ger/loadproducts/viewmodel/ProductsViewModel.kt
@@ -1,5 +1,7 @@
 package com.ger.loadproducts.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ger.loadproducts.data.ProductRepository
@@ -18,14 +20,14 @@ class ProductsViewModel
 @Inject
 constructor(private val productRepository: ProductRepository) : ViewModel() {
 
-    private val _productState = MutableStateFlow(
+    private val _productState = MutableLiveData(
         ProductState(
             products = listOf(
-                Product("Toalla", "Toalla de algodon 80 x 120 cm", 299.9)
+                Product("Toalla", "Toalla de algodón 80 x 120 cm", 299.9)
             )
         )
     )
-    val productState = _productState.asStateFlow()
+    val productState: LiveData<ProductState> = _productState
 
     val exceptionHandler = CoroutineExceptionHandler { _, exception ->
         println("¡Error no capturado!: ${exception.message}")
@@ -35,13 +37,9 @@ constructor(private val productRepository: ProductRepository) : ViewModel() {
         viewModelScope.launch(Dispatchers.IO + exceptionHandler) {
             try {
                 val nuevaLista = productRepository.getProducts()
-                _productState.update {
-                    it.copy(products = nuevaLista, isLoading = false)
-                }
+                _productState.postValue(_productState.value?.copy(products = nuevaLista, isLoading = false))
             } catch (e: Exception) {
-                _productState.update {
-                    it.copy(isLoading = false, error = e)
-                }
+                _productState.postValue(_productState.value?.copy(error = e, isLoading = false))
             }
         }
     }
